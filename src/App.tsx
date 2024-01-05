f3d89617353e610544ab2ff62d8f4440325587c3
Humberto <humbertomartinez900519@gmail.com>
2024-01-05 16:46:46 +0300

    using line to draw the edges

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,19 @@ function App() {
       <Vertice label={1} ref={firstItemRef} />
       <Vertice label={2} ref={secondItemRef} />
       <Vertice label={3} ref={thirdItemRef} />
-      <Line div1Ref={firstItemRef} div2Ref={secondItemRef} />
-      <Line div1Ref={firstItemRef} div2Ref={thirdItemRef} />
-      <Line div1Ref={secondItemRef} div2Ref={thirdItemRef} />
+      <svg
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Line div1Ref={firstItemRef} div2Ref={secondItemRef} />
+        <Line div1Ref={firstItemRef} div2Ref={thirdItemRef} />
+        <Line div1Ref={secondItemRef} div2Ref={thirdItemRef} />
+      </svg>
     </div>
   );
 }
