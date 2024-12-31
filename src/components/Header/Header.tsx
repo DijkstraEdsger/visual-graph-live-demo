ae1fa976442522109264397e16ca5b3df144cb37
Humberto <humbertomartinez900519@gmail.com>
2024-12-31 20:40:33 +0300

    add logo to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,12 +3,16 @@ import useHeaderVM from "./useHeaderVM";
 import classes from "./classes.module.scss";
 import MenuBar from "components/MenuBar";
 import ThemeButton from "./ThemeButton";
+import LogoIcon from "components/Icon/Icons/LogoIcon";
 
 const Header: React.FC = () => {
   const { menus } = useHeaderVM();
 
   return (
     <header className={classes.header}>
+      <div className={classes.logo}>
+        <LogoIcon />
+      </div>
       <MenuBar menus={menus} />
       <ThemeButton />
     </header>
