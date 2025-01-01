92e0db52c52281ec408f5546f187ca8ef7d3dc87
Humberto <humbertomartinez900519@gmail.com>
2025-01-01 20:28:15 +0300

    Wrap the app logo with a link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,15 +4,16 @@ import classes from "./classes.module.scss";
 import MenuBar from "components/MenuBar";
 import ThemeButton from "./ThemeButton";
 import LogoIcon from "components/Icon/Icons/LogoIcon";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   const { menus } = useHeaderVM();
 
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>
+      <Link to="/" title="Graph Builder" className={classes.logo}>
         <LogoIcon />
-      </div>
+      </Link>
       <MenuBar menus={menus} />
       <ThemeButton />
     </header>
